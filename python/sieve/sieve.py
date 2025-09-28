f31106043cb1290afc5fb1533fd57ea26943aeb3
helliot2 <helliot@revenova.com>
2025-09-28 16:34:01 -0500

    sieve solution

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,26 @@
+from math import log, ceil
 class Sieve:
     def __init__(self) -> None:
         pass
+
     def nth_prime(self, n: int) -> int:
-        pass
+        if n < 0:
+            raise ValueError("n must be greater than 0")
+        if n < 6:
+            limit = 12
+        else:
+            limit  = ceil(n*log(n*log(n)))
+        while True:
+            primes = list(self.primes_up_to(limit))
+            if len(primes) > n:
+                return primes[n]
+            print('we got here ', limit)
+            limit *= 2
+
+    def primes_up_to(self, limit):
+        prime = [True] * limit
+        for i in range(2, limit):
+            if(prime[i]):
+                yield i
+                for c in range(i*i, limit, i):
+                    prime[c] = False
